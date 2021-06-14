2db4e8780aa8e6433b8d737fd0d9bfaa4d3a7ccd
drakumus <rohancheeniyil@gmail.com>
2021-06-14 16:04:27 -0600

    attempt to automate release of docker image onto remote server

---
+++ .github/workflows/deploy.sh
@@ -0,0 +1,5 @@
+ssh -i /mnt/c/Users/Rohan/Documents/rsa/terraform/project_server root@68.183.51.26
+
+# login to github: echo "GITHUB_TOKEN" | docker login ghcr.io -u drakumus --password-stdin
+# docker pull ghcr.io/drakumus/website-image:latest
+# docker compose up
