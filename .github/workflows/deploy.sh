c1016b8a6bf548a479e72cd4726799801322e418
drakumus <rohancheeniyil@gmail.com>
2021-06-14 20:52:43 -0600

    change nginx conf for new docker build

--- .github/workflows/deploy.sh
+++
@@ -1,5 +0,0 @@
-ssh -i /mnt/c/Users/Rohan/Documents/rsa/terraform/project_server root@68.183.51.26
-
-# login to github: echo "GITHUB_TOKEN" | docker login ghcr.io -u drakumus --password-stdin
-# docker pull ghcr.io/drakumus/website-image:latest
-# docker compose up
