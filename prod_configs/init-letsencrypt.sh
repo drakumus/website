6a7d60ec627e50ed2efa7403b961a93cb09da18a
drakumus <rohancheeniyil@gmail.com>
2021-06-16 18:22:12 -0600

    are we there yet?

--- prod_configs/init-letsencrypt.sh
+++ prod_configs/init-letsencrypt.sh
@@ -5,17 +5,14 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(zoci.me, www.zoci.me)
+domains=(zoci.me www.zoci.me)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="rohancheeniyil@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-    exit
-  fi
+  exit
 fi
 
 
@@ -77,4 +74,4 @@ docker-compose run --rm --entrypoint "\
 echo
 
 echo "### Reloading zocimebby_website ..."
-docker-compose exec zocimebby_website nginx -s reload
+docker-compose exec website nginx -s reload
