4cd1a7e476909a0b60c23e6bd287b6f21cca5030
drakumus <rohancheeniyil@gmail.com>
2021-06-16 18:15:18 -0600

    attempt to fix routing

--- prod_configs/init-letsencrypt.sh
+++ prod_configs/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(zoci.me www.zoci.me)
+domains=(zoci.me, www.zoci.me)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="rohancheeniyil@gmail.com" # Adding a valid address is strongly recommended
