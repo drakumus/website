27f14b31b7d36e0908247781230c46076ba32ed7
drakumus <rohancheeniyil@gmail.com>
2022-04-12 18:07:52 -0600

    Change how we log upgrade loot

--- website/src/react/drops/src/DropsTable.js
+++ website/src/react/drops/src/DropsTable.js
@@ -1,7 +1,7 @@
 import './drops_style.css';
 import React, {Component} from "react";
 
-function getPlayerItemReq(item_type, player_id, bis_data, item_types)
+function getPlayerBis(player_id, bis_data)
 {
   let player_bis = {}
   for(let index in bis_data)
@@ -11,17 +11,29 @@ function getPlayerItemReq(item_type, player_id, bis_data, item_types)
       player_bis = bis_data[index]
     }
   }
+
   if(Object.keys(player_bis).length === 0)
   {
     console.log("Could not find player_id: " + player_id)
-    return false;
   }
+
+  return player_bis
+}
+
+
+function getPlayerItemReq(item_type, player_bis, item_types)
+{
   if(item_type > 9)
   {
     return false;
   } else
   {
-    let item_key_name = `uses_raid_${item_types[item_type].toLowerCase()}`
+    let item_name = item_types[item_type].toLowerCase();
+    if(item_type == 9)
+    {
+      item_name = "ring1"
+    }
+    let item_key_name = `uses_raid_${item_name}`
     return player_bis[item_key_name] != 0
   }
 }
@@ -55,10 +67,25 @@ class DropsTable extends Component {
         for( let name_index in this.props.static_members )
         {
           let player_drops = item_drops.filter( (player_drop) => { return player_drop.player_id === this.props.static_members[name_index].id } )
-          let is_required = getPlayerItemReq(item_type_index, this.props.static_members[name_index].id, this.props.bis, this.props.item_types)
-          if(player_drops.length > 0)
+          let player_bis = getPlayerBis(this.props.static_members[name_index].id, this.props.bis)
+          let is_required = getPlayerItemReq(item_type_index, player_bis, this.props.item_types)
+          if(player_drops.length > 0 || item_type_index > 9)
           {
-            if (item_type_index > 9 || !this.props.is_main) // dusting, twine, ether
+            if (item_type_index > 9 && this.props.is_main) // twine, ether, etc
+            {
+              // case where item isn't needed and no drops received in main drop table
+              let item_key_name = `num_${this.props.item_types[item_type_index].toLowerCase()}_needed`
+              let num_deficit = player_drops.length - player_bis[item_key_name] // note this is a negative normally
+              if(player_drops.length == 0 && player_bis[item_key_name] == 0)
+              {
+                item_td.push((<td>-</td>))
+              }
+              else
+              {
+                item_td.push((<td class="styled-table-text">{num_deficit >= 0 ? '✔️': num_deficit}</td>))
+              }
+            }
+            else if (item_type_index > 9 || !this.props.is_main) // alt drops table
             {
               item_td.push((<td class="styled-table-text">{player_drops.length}</td>))
             }
